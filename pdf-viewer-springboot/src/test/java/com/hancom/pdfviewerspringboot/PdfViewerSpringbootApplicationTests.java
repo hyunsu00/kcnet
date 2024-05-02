dfe56a8e72cac7ecca44cc90b818a7afd9bbea08
hyunsu00 <hyunsu00@hancom.com>
2024-05-02 23:59:30 +0900

    업데이트

---
+++ pdf-viewer-springboot/src/test/java/com/hancom/pdfviewerspringboot/PdfViewerSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hancom.pdfviewerspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfViewerSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
